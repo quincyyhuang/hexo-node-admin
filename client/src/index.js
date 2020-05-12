aa89d9be563fb58fc11a5bb497d51f19d23d2f36
Quincy Huang <quincyyhuang@gmail.com>
2020-05-12 01:48:09 -0400

    disable react dev tool

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,18 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 
+// Disable React Dev Tool on production
+if (process.env.NODE_ENV === 'production')
+{
+  const noop = () => undefined; // Empty function
+  const DEV_TOOLS = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+  if (typeof DEV_TOOLS === 'object') {
+    for (const [key, value] of (Object).entries(DEV_TOOLS)) {
+      DEV_TOOLS[key] = typeof value === 'function' ? noop : null;
+    }
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
